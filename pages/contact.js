cbc498fa344f83bae3c5fd8eb4c17dc554a9833a
Ushankar-1 <53460303+Ushankar-1@users.noreply.github.com>
2022-02-19 12:12:31 -0800

    Began basic front-end

--- pages/contact.js
+++ pages/contact.js
@@ -1,8 +1,14 @@
+import styles from '../styles/Home.module.css'
+import Head from 'next/head'
+
 function Contact() {
     return (
-        <div>
-            
-        </div>
+      <div className={styles.container}>
+        <Head>
+          <title>"Questions? Reach Out!"</title>
+        </Head>
+        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"</p>
+      </div>
     )
 }
 
