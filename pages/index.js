16a7df357cc2fe5e7bd9dc11b7edba89941067d5
brandonw504 <71607013+brandonw504@users.noreply.github.com>
2022-03-03 15:07:32 -0800

    basic mobile view

--- pages/index.js
+++ pages/index.js
@@ -10,16 +10,27 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className={styles.wrapper}>
       <Head>
         <title>Artruism at UC Davis</title>
         <meta name="description" content="Artruism is a club at UC Davis committed to drawing for a difference."></meta>
       </Head>
+<<<<<<< Updated upstream
       <About />
       <Gallery />
       <Calendar />
       <Join />
       <Contact />
+=======
+      <div className={styles.container}>
+        <Nav />
+        <About />
+        <Gallery />
+        <Calendar />
+        <Join />
+        <Contact />
+      </div>
+>>>>>>> Stashed changes
     </div>
   )
 }
